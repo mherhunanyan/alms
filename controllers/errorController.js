6fba790850d038041700a7cf8ff019ea86488828
Mher Hunanyan <mher.hunanyan2@gmail.com>
2024-01-15 21:26:15 +0400

    bug fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,10 @@
 
 const get404 = (req, res, next) => {
-  res.render("errors/404", { pageTitle: "Page Not Found" });
+  res.render("errors/404", { pageTitle: "Page Not Found", page: "/404"});
 };
 
 const get500 = (err, req, res, next) => {
-  res.render("errors/500", { pageTitle: "500 Error" });
+  res.render("errors/500", { pageTitle: "500 Error", page: "/500"});
 };
 
 module.exports = {
--- middleware/isAdminMiddleware.js
+++ middleware/isAdminMiddleware.js
@@ -1,6 +1,6 @@
 const isAdminMiddleware = (req, res, next) => {
   if (!req.isAdmin) {
-    res.render("errors/404", { pageTitle: "Page Not Found" });
+    res.render("errors/404", { pageTitle: "Page Not Found", page: "/404" });
   }
   next();
 };
